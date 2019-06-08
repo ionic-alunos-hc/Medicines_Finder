86aadc28137b3113179db8e84dfc65e3d17bf2f2
ionic-alunos-hc <quevin.happycode@gmail.com>
2019-06-08 13:03:57 +0100

    second update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MedicinesPage } from '../pages/medicines/medicines';
+import { ItemsProvider } from '../providers/items/items';
+import { ApiProvider } from '../providers/api/api';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    MedicinesPage,
     TabsPage
   ],
   imports: [
@@ -29,12 +33,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MedicinesPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ItemsProvider,
+    ApiProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/benuron/benuron.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { BenuronPage } from './benuron';
+
+@NgModule({
+  declarations: [
+    BenuronPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BenuronPage),
+  ],
+})
+export class BenuronPageModule {}
